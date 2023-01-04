f743c93bbb93a9e3e4000d7352ed957fc1ed4a8d
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2023-01-04 19:18:47 +0600

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,5 @@
 using System.Collections;
 using UnityEngine;
-using Quaternion = UnityEngine.Quaternion;
-using Vector2 = UnityEngine.Vector2;
-using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
